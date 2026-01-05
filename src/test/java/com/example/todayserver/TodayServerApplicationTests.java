ebf2269ec293f85132686f400a7678ce0ba95225
Juri-Jang <jjr84@dgu.ac.kr>
2026-01-05 21:50:16 +0900

    init : 프로젝트 초기 세팅

---
+++ src/test/java/com/example/todayserver/TodayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
