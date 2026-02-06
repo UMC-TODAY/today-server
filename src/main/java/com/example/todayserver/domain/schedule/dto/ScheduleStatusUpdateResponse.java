e313e42ac3774011bdd8bf5fb2edff65e7d47447
SihwanGit <sihwan1214@naver.com>
2026-02-06 21:27:22 +0900

    fix : 상태 변경 및 완료 처리 버그 수정

--- src/main/java/com/example/todayserver/domain/schedule/dto/ScheduleStatusUpdateResponse.java
+++ src/main/java/com/example/todayserver/domain/schedule/dto/ScheduleStatusUpdateResponse.java
@@ -1,14 +1,17 @@
 package com.example.todayserver.domain.schedule.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-
 // 일정/할일 상태변경 및 완료 처리용 dto
 public class ScheduleStatusUpdateResponse {
 
     private Long id;
-    private boolean is_done;
+
+    // 응답 JSON은 is_done, 자바 필드는 isDone
+    @JsonProperty("is_done")
+    private boolean isDone;
 }
