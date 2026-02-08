0139a874e174e7883870b11b1ac4e68b276f6ba0
Juri-Jang <jjr84@dgu.ac.kr>
2026-02-08 22:27:54 +0900

    chore : 스웨거 https 설정 추가

--- src/main/java/com/example/todayserver/global/config/SwaggerConfig.java
+++ src/main/java/com/example/todayserver/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.example.todayserver.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -7,6 +9,11 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://today-app.co.kr", description = "개발 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 서버")
+        })
 @Configuration
 public class SwaggerConfig {
 
@@ -23,7 +30,6 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(new Info()
-                        .title("TODAY API 문서")
                         .description("TODAY 프로젝트의 REST API 명세서입니다.")
                         .version("v1.0.0"))
                 .addSecurityItem(securityRequirement)
