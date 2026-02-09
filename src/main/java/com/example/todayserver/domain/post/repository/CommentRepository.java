4c1e7618fdbfc8e2246508ea2696108198f1e5c7
jm-pad <dkssud2271@naver.com>
2026-02-09 15:26:07 +0900

    [Refactor] 내활동 누적 수 수정#33

--- src/main/java/com/example/todayserver/domain/post/repository/CommentRepository.java
+++ src/main/java/com/example/todayserver/domain/post/repository/CommentRepository.java
@@ -1,10 +1,16 @@
 package com.example.todayserver.domain.post.repository;
 
+import com.example.todayserver.domain.member.entity.Member;
 import com.example.todayserver.domain.post.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByPostIdOrderByCreatedAtDesc(Long postId);
+
+    @Query("SELECT COUNT(c) FROM Comment c WHERE c.post.member = :member")
+    Long countAllCommentsByPostMember(@Param("member") Member member);
 }
--- src/main/java/com/example/todayserver/domain/post/repository/PostLikeRepository.java
+++ src/main/java/com/example/todayserver/domain/post/repository/PostLikeRepository.java
@@ -4,9 +4,14 @@ import com.example.todayserver.domain.member.entity.Member;
 import com.example.todayserver.domain.post.entity.Post;
 import com.example.todayserver.domain.post.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     Optional<PostLike> findByMemberAndPost(Member member, Post post);
+
+    @Query("SELECT COUNT(pl) FROM PostLike pl WHERE pl.post.member = :member")
+    Long countAllLikesByPostMember(@Param("member") Member member);
 }
